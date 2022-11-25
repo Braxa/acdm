3f43d443aac56c13e0403d2ce46c00551d8650ef
Braxa <31308629+Braxa@users.noreply.github.com>
2022-11-26 00:01:38 +0100

    up

--- acdm.js
+++ acdm.js
@@ -4,7 +4,7 @@
  Hooks.once("init", function(){
 	 console.log("acdm | Initialising Academic system");
 	 
-	 CONFIG.acdm = acdm;
+	 //CONFIG.acdm = acdm;//
 	 
 	 Items.unregisterSheet("core", ItemSheet);
 	 Items.registerSheet("acdm", acdmItemSheet, {makeDefault:true});
